b43e6ef16a6cdd8c89ae0de666eec62e746226c4
MutumaDerick <pdmutuma@gmail.com>
2024-09-03 23:47:11 +0300

    Added Input decorations

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,55 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final border = OutlineInputBorder(
+        borderSide: const BorderSide(
+          color: Colors.blueAccent,
+          width: 2,
+          style: BorderStyle.solid,
+        ),
+        borderRadius: BorderRadius.circular(60));
+
+    return Scaffold(
       backgroundColor: Colors.white54,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               '0',
-              style: TextStyle(
+              style: const TextStyle(
                   fontSize: 48,
                   fontWeight: FontWeight.bold,
                   color: Colors.blueAccent),
             ),
             TextField(
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 24,
                 color: Colors.black,
               ),
               decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blueAccent,
-                    width: 2,
-                    style: BorderStyle.solid,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(20),
-                  ),
-                ),
                 hintText: 'Enter amount',
-                prefixIcon: Icon(Icons.monetization_on),
-                prefixIconColor: Color.fromARGB(191, 0, 0, 0),
+                hintStyle: const TextStyle(
+                  fontSize: 24,
+                  color: Colors.black,
+                ),
+                prefixIcon: const Icon(Icons.monetization_on),
+                prefixIconColor: const Color.fromARGB(191, 0, 0, 0),
                 filled: true,
                 fillColor: Colors.white,
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blueAccent,
-                    width: 2,
-                    style: BorderStyle.solid,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(20),
-                  ),
-                ),
+                focusedBorder: border,
+                enabledBorder: border,
               ),
               keyboardType: TextInputType.number,
             ),
