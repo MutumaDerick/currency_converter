870fcbc8ccb2f0bdd469430583b7237eef8e7ce3
MutumaDerick <pdmutuma@gmail.com>
2024-09-03 22:11:23 +0300

    Added widgets

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -9,20 +9,47 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
       backgroundColor: Colors.white54,
       body: Center(
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text('0',
-           style: TextStyle(
-            fontSize: 48,
-            fontWeight: FontWeight.bold,
-            color: Colors.blueAccent
-             ),
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueAccent),
             ),
             TextField(
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.black,
+              ),
               decoration: InputDecoration(
-                labelText: 'Enter amount',
-                border: OutlineInputBorder(),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blueAccent,
+                    width: 2,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                hintText: 'Enter amount',
+                prefixIcon: Icon(Icons.monetization_on),
+                prefixIconColor: Color.fromARGB(191, 0, 0, 0),
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blueAccent,
+                    width: 2,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
               ),
               keyboardType: TextInputType.number,
             ),
